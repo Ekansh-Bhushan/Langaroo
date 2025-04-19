4207a0693ee2116f7ad861bd95e0cc020f84fbd9
Ekansh Bhushan <ekanshbhushan2k22@gmail.com>
2025-04-19 19:53:26 +0530

    learn page wrapper

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,16 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+
 const Learn = () => {
     return (
-        <div>
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+
             Leanr Page
+            </StickyWrapper>
+            <FeedWrapper>
+                <div className="h-[200px] w-full bg-blue-500" />
+            </FeedWrapper>
         </div>
     )
 }
