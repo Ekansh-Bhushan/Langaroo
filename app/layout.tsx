61ccd977d6b6a38817ac8f67ea36c92f9c7facc0
Ekansh Bhushan <ekanshbhushan2k22@gmail.com>
2025-04-20 12:44:53 +0530

    userprogress

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const nunito = Nunito({
@@ -22,6 +23,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${nunito.variable} antialiased`}>
+          <Toaster />
           {children}
         </body>
       </html>
