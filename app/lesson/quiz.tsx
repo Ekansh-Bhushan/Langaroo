b0cd3ddc80d94ab56bdfacad65a82fe0741a2c0f
Ekansh Bhushan <ekanshbhushan2k22@gmail.com>
2025-04-28 10:07:14 +0530

    quiz imrpoved

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -32,9 +32,15 @@ export const Quiz = ({
         const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed)
         return uncompletedIndex === -1 ? 0 : uncompletedIndex;
     });
+    const [selectedOption, setSelectedOption] = useState<number>();
+    const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
     const challenge = challenges[activeIndex];
     const options = challenge?.challengeOptions ?? [];
 
+    const onSelect = (id:number) => {
+        if(status !== "none") return;
+        setSelectedOption(id);
+    }
     const title = challenge.type === "ASSIST" 
     ? "Select the correct meaning"
     : challenge.question;
@@ -59,9 +65,9 @@ export const Quiz = ({
                         )}
                         <Challenge
                             options={options}
-                            onSelect={() => {}}
-                            status="none"
-                            selectedOption={undefined}
+                            onSelect={onSelect}
+                            status={status}
+                            selectedOption={selectedOption}
                             disabled={false}
                             type={challenge.type}
                         />
@@ -70,6 +76,7 @@ export const Quiz = ({
                 </div>
             </div>
         </div>
+        
         </>
     )
 }
