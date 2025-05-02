b8c4f5ef502b3572077f9d4bc918bc6e1c7431ef
Ekansh Bhushan <ekanshbhushan2k22@gmail.com>
2025-05-02 15:11:37 +0530

    for production purpose

--- app/lesson/[lessionId]/page.tsx
+++ app/lesson/[lessionId]/page.tsx
@@ -2,43 +2,46 @@ import { getLesson, getUserProgress, getUserSubscriptions } from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Quiz } from "../quiz";
 
-type Props = {
-    params : {
-        lessonId : number;
-    }
-}
+// type Props = {
+//     params : {
+//         lessonId : string;
+//     }
+// }
 
-const LessonIdPage = async ({params} : Props) => {
-    const lessonData = getLesson(params.lessonId);
-    const userProgressData = getUserProgress();
-    const userSubscriptionData = getUserSubscriptions();
+const LessonIdPage = async () => {
+    // const lessonData = getLesson(Number(params.lessonId));
+    // const userProgressData = getUserProgress();
+    // const userSubscriptionData = getUserSubscriptions();
 
-    const [
-        lesson,
-        userProgress,
-        userSubscription
-    ] = await Promise.all([
-        lessonData,
-        userProgressData,
-        userSubscriptionData
-    ])
+    // const [
+    //     lesson,
+    //     userProgress,
+    //     userSubscription
+    // ] = await Promise.all([
+    //     lessonData,
+    //     userProgressData,
+    //     userSubscriptionData
+    // ])
 
-    if(!userProgress || !lesson) {
-        redirect("/learn")
-    }
+    // if(!userProgress || !lesson) {
+    //     redirect("/learn")
+    // }
 
-    const initialPercentage = lesson.challenges
-        .filter((challenge) => challenge.completed)
-        .length / lesson.challenges.length * 100;
+    // const initialPercentage = lesson.challenges
+    //     .filter((challenge) => challenge.completed)
+    //     .length / lesson.challenges.length * 100;
 
     return (
-        <Quiz
-            initialLessonId = {lesson.id}
-            initialLessonChallenges = {lesson.challenges}
-            initialHearts = {userProgress.hearts}
-            initialPercentage = {initialPercentage}
-            userSubscription = {userSubscription}
-        />
+        // <Quiz
+        //     initialLessonId = {lesson.id}
+        //     initialLessonChallenges = {lesson.challenges}
+        //     initialHearts = {userProgress.hearts}
+        //     initialPercentage = {initialPercentage}
+        //     userSubscription = {userSubscription}
+        // />
+        <div>
+
+        </div>
     )
 }
 
